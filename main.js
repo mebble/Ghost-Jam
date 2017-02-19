1becbd30cebfbb83fb9a70abb0da100f84638d05
Neil Syiemlieh <iit2016125@iiita.ac.in>
2017-02-19 14:03:29 +0530

    Coordinate offsets and distance

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
-draw = function() {
-    pushMatrix();
-    translate(width/2, height/2);
-    
-    
-    popMatrix();
+var XOFF = width/2;
+var YOFF = height/2;
+angleMode = "degrees";
+
+var distance = function(a, b) {
+	return dist(a.x, a.y, b.x, b.y);
 };
+
+draw = function() {
+	background(255);
+	pushMatrix();
+		translate(XOFF, YOFF);
+	popMatrix();
+}
